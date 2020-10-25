90739adc9a12a4b882f8cb99678523073ad3ae76
momayaz <momayaz.dr.1@gmail.com>
2020-10-25 15:14:19 +0300

    lab02

--- js/app.js
+++ js/app.js
@@ -1,42 +1,96 @@
-
-
 'use strict';
 
-function Photos(data) {
-  this.image = data.image_url;
-  this.title = data.title;
-  this.descrip = data.description;
-  this.keyWord = data.keyword;
-  this.horn = data.horns;
+
+function Horns (data) {
+  this.image_url = data.image_url,
+  this.title = data.title,
+  this.description = data.description,
+  this.keyword = data.keyword,
+  this.horns = data.horns;
+
 }
 
-$.ajax('../data/page-1.json', {
+Horns.prototype.render = function () {
+
+  let photoTemplateCloned = $('#photo-template').clone();
+  $('main').append(photoTemplateCloned);
+  //   photoTemplateCloned.className=this.keyword;
+  //   photoTemplateCloned.id = this.keyword;
+
+  photoTemplateCloned.html (
+
+    `   
+        <div class = "${this.keyword}">
+        <h2>${this.title}</h2>
+        <img src="${this.image_url}" alt="">
+        <p id="dec">${this.description}</p>
+        <p id="numHorns">number of horns is ${this.horns}</p>
+        </div>
+        
+    `
+  );
+
+  //   photoTemplateCloned.find('h2').text(this.title);
+  //   photoTemplateCloned.find('img').attr('src',this.image_url);
+  //   photoTemplateCloned.find('#dec').text(this.description);
+  //   photoTemplateCloned.find('#numHorns').text('number of horns: ' + this.horns);
+
+};
+
+const ajaxSettings = {
   method: 'get',
-  dataType: 'json',
-}).then(allData => {
-  allData.forEach(allInfo => {
-    let allPhotos = new Photos(allInfo);
-    allPhotos.render();
-    console.log(allPhotos);
+  dataType: 'json'
+};
+
+$.ajax ('data/page-1.json',ajaxSettings).then ( data => {
+  data.forEach(element => {
+
+    let horn = new Horns (element);
+    horn.render();
+
+    let optionEl = $(`<option value = "${horn.keyword}">${horn.keyword}</option>`);
+    $('select').append(optionEl);
+
+
+  });
+
+
+
+});
+
+$(document).ready(function(){
+  $('select').on('change', function(event){
+
+    let selected = event.target.value; // selected = value === className
+    console.log(selected);
+
+    $(`div`).hide();
+    $(`.${selected}`).fadeIn(1000);
+
   });
 });
-Photos.prototype.render = function () {
-  let selectorEl = $('<option></option>').text(this.keyWord);
-  $('select').append(selectorEl);
-  let sectionEl = $('#section-template').clone();
-  $('main').append(sectionEl);
-  sectionEl.find('h2').text(this.title);
-  sectionEl.find('img').attr('src', this.image);
-  sectionEl.find('#description').text(this.descrip);
-  sectionEl.find('#horns').text(`The number Of Horns Is: ${this.horn}`);
-  sectionEl.attr('class', this.keyWord);
 
 
-}
 
-$('select').change(function () {
-  let selected = this.value;
-  $('section').hide();
-  $(`.${selected}`).show();
-  console.log(selected);
-});
+// $(document).ready(function(){
+
+//   all.forEach(element => {
+//     let optionEl = `<option>${element.title}</option>`;
+//     console.log(optionEl);
+//     $('select').append('optionEl');
+//   });
+
+// });
+
+
+
+// for (let i = 0; i < all.length; i++) {
+//   let optionEl = $('<option>' + all[i].title + '</option>');
+//   $('main').append('select');
+// }
+
+// all.forEach(element => {
+//   let optionEl = `<option>${element.title}</option>`;
+//   console.log(optionEl);
+//   $('select').append('optionEl');
+// });
